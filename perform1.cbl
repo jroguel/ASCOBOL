dffb1639cfd8ad91351bb914cc4a16ca90381ff5
Jairus Roguel <jairuz1011@gmail.com>
2017-01-31 11:05:12 +0800

    added new exercises

---
+++ perform1.cbl
@@ -0,0 +1,30 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  PerformFormat1.
+       PROCEDURE DIVISION.
+       TopLevel.
+           DISPLAY "In TopLevel. Starting to run program"
+           PERFORM OneLevelDown
+           DISPLAY "Back in TopLevel.".
+       STOP RUN.
+
+
+       TwoLevelsDown.
+           DISPLAY ">>>>>>>> Now in TwoLevelsDown."
+           PERFORM ThreeLevelsDown.
+           DISPLAY ">>>>>>>> Back in TwoLevelsDown.".
+
+
+       OneLevelDown.
+           DISPLAY ">>>> Now in OneLevelDown"
+           PERFORM TwoLevelsDown
+           DISPLAY ">>>> Back in OneLevelDown".
+
+
+       ThreeLevelsDown.
+           DISPLAY ">>>>>>>>>>>> Now in ThreeLevelsDown".
