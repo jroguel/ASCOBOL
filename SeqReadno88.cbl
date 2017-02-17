7efcfe037c96c16b768f872d780bb09318e75770
Jairus Roguel <jairuz1011@gmail.com>
2017-02-17 11:16:04 +0800

    Added Sequential Files

---
+++ SeqReadno88.cbl
@@ -0,0 +1,44 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SeqReadno88.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT StudentFile ASSIGN TO "STUDENTS.DAT"
+		                    ORGANIZATION IS LINE SEQUENTIAL.
+       DATA DIVISION.
+       FILE SECTION.
+       FD StudentFile.
+       01 StudentDetails.
+           02  StudentId       PIC 9(7).
+           02  StudentName.
+               03 Surname      PIC X(8).
+               03 Initials     PIC XX.
+           02  DateOfBirth.
+               03 YOBirth      PIC 9(4).
+               03 MOBirth      PIC 9(2).
+               03 DOBirth      PIC 9(2).
+           02  CourseCode      PIC X(4).
+           02  Gender          PIC X.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       Begin.
+           OPEN INPUT StudentFile
+           READ StudentFile
+               AT END MOVE HIGH-VALUES TO StudentDetails
+           END-READ
+           PERFORM UNTIL StudentDetails = HIGH-VALUES
+               DISPLAY StudentId SPACE StudentName SPACE CourseCode
+                       SPACE YOBirth
+               READ StudentFile
+                   AT END MOVE HIGH-VALUES TO StudentDetails
+               END-READ
+           END-PERFORM
+           CLOSE StudentFile
+           STOP RUN.
+       END PROGRAM SeqReadno88.
---
+++ SeqWrite.cbl
@@ -0,0 +1,50 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SeqWrite.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT StudentFile ASSIGN TO "STUDENTS.DAT"
+		                    ORGANIZATION IS LINE SEQUENTIAL.
+       DATA DIVISION.
+       FILE SECTION.
+       FD StudentFile.
+       01 StudentDetails.
+           02  StudentId       PIC 9(7).
+           02  StudentName.
+               03 Surname      PIC X(8).
+               03 Initials     PIC XX.
+           02  DateOfBirth.
+               03 YOBirth      PIC 9(4).
+               03 MOBirth      PIC 9(2).
+               03 DOBirth      PIC 9(2).
+           02  CourseCode      PIC X(4).
+           02  Gender          PIC X.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       Begin.
+           OPEN OUTPUT StudentFile
+           DISPLAY "Enter student details using template below.
+           Enter no data to end."
+
+           PERFORM GetStudentDetails
+           PERFORM UNTIL StudentDetails = SPACES
+               WRITE StudentDetails
+               PERFORM GetStudentDetails
+           END-PERFORM
+           CLOSE StudentFile
+           STOP RUN.
+
+
+       GetStudentDetails.
+           DISPLAY "Enter - StudId, Surname, Initials, YOB, MOB, DOB,
+           Course, Gender"
+           DISPLAY "NNNNNNNSSSSSSSSIIYYYYMMDDCCCCG"
+           ACCEPT  StudentDetails.
+
+       END PROGRAM SeqWrite.
